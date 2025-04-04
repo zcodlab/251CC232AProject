c82f8f00341a90b5aa1ed5c00fa04ea1d7e06e26
zcodlab <zmamanir@uni.edu.pe>
2025-04-04 10:07:29 -0500

    modify SearchMain class

--- src/main/java/uni/aed/search/SearchMain.java
+++ src/main/java/uni/aed/search/SearchMain.java
@@ -1,29 +1,76 @@
 package uni.aed.search;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import uni.aed.sort.Sort;
 
 public class SearchMain {
+    private static final Scanner scr=new Scanner(System.in).useDelimiter("\n");
     private static Search search;
+    private static Sort sort;
+    private Integer[] X=null;
     
     public static void main(String[] args){
         SearchMain searchMain=new SearchMain();
         search=new Search();
-        searchMain.carga();
-        searchMain.visualizar();
-        //searchMain.callSearchLineal();
-        searchMain.callSearchBinaria();
+        sort=new Sort();
+        searchMain.menu();        
     }
+    private void menu(){
+        String SEPARADOR="\n";
+        int opcion=3;
+        try{
+            do{
+                System.out.println("Algoritmos de Busqueda y Ordenamiento " +SEPARADOR+
+                    "1.-Carga Fija de Datos en Array "+SEPARADOR+
+                    "2.-Carga Personalizada de Datos en Array "+SEPARADOR+
+                    "3.-Visualizar Array "+SEPARADOR+
+                    "4.-Busqueda Lineal "+SEPARADOR+
+                    "5.-Busqueda Binaria "+SEPARADOR+
+                    "6.-Ordenamiento Burbuja "+SEPARADOR+
+                    "7.-Ordenamiento Seleccion "+SEPARADOR+
+                    "8.-Salir "+SEPARADOR+ "Elija una opcion:");
+                opcion=scr.nextInt();
+                switch(opcion){
+                    case 1->{carga();visualizar();}
+                    case 2->{cargaPersonalizada();visualizar();}
+                    case 3->{visualizar();}                    
+                    case 4->{callSearchLineal();}                    
+                    case 5->{callSearchBinaria();}                    
+                    case 6->{callBubbleWuSort();}                    
+                    case 7->{callSelectionWuSort();}                    
+                    case 8->{System.out.println("Saliendo..."); return;}//salir del programa
+                    default->System.out.println("Opcion Invalida....");
+                }                
+            }while(opcion!=8);            
+        }catch(InputMismatchException e){
+            System.out.println("Debe ingresar un numero "+ e.toString());
+        }catch(Exception e){
+            System.out.println("Se presento un error "+ e.toString());
+        }
+    }
+    
     private void carga(){
-        Integer[] X={15,1,25,60,69,86,3,78,2,10};
+        Integer[] Y={15,1,25,60,69,86,3,78,2,10};
+        X=Y.clone();
         search.setX(X);
     }
+    private void cargaPersonalizada(){
+        System.out.println("Ingrese el tamaño del array");
+        int N=scr.nextInt();
+        X=new Integer[N];
+        for(int i=0;i<X.length;i++){
+            System.out.println("Ingrese el valor X["+i+"]=");
+            X[i]=scr.nextInt();
+        }
+        search.setX(X);
+    }
+    
     private void visualizar(){
         System.out.println("El array contiene los siguientes elementos:");
         System.out.println(search.toString());
     }
-    private void callSearchLineal(){
-        Scanner scr=new Scanner(System.in);
+    private void callSearchLineal(){        
         System.out.println("Ingrese el valor a buscar:");
         int valor=scr.nextInt();
         int pos=search.searchLineal(valor);
@@ -32,8 +79,7 @@ public class SearchMain {
         else
             System.out.println("El valor buscado "+valor+" se ubico en la posicion "+ pos);                    
     }
-    private void callSearchBinaria(){
-        Scanner scr=new Scanner(System.in);
+    private void callSearchBinaria(){        
         System.out.println("Ingrese el valor a buscar:");
         int valor=scr.nextInt();
         //callBubbleWuSort();
@@ -46,17 +92,17 @@ public class SearchMain {
         
     }
     
-    private void callBubbleWuSort(){
-        Sort sort=new Sort();
-        sort.setX(search.getX());
+    private void callBubbleWuSort(){  
+        Integer[] Y=X.clone();
+        sort.setX(Y);//pasando el array origen
         System.out.println("Array inicial: " + sort.toString());
         sort.bubbleWuSort();
         System.out.println("Array final: " + sort.toString());
     }
     
-    private void callSelectionWuSort(){
-        Sort sort=new Sort();
-        sort.setX(search.getX());
+    private void callSelectionWuSort(){        
+        Integer[] Y=X.clone();
+        sort.setX(Y);//pasando el array origen
         System.out.println("Array inicial: " + sort.toString());
         sort.selectionWuSort();
         System.out.println("Array final: " + sort.toString());
