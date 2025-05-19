bb293e7d5c3f81a6861675eb9f5c092ab4aeaf6d
zcodlab <zmamanir@uni.edu.pe>
2025-05-19 13:12:25 -0500

    remove1 cambio nombre packages

--- src/main/java/uni/aed/tda/ContenedorMagico.java
+++
@@ -1,6 +0,0 @@
-package uni.aed.tda;
-
-public interface ContenedorMagico<T> {
-    public T get();
-    public void set(T contenido);    
-}
--- src/main/java/uni/aed/tda/listTDA/IteratorTDA.java
+++
@@ -1,6 +0,0 @@
-package uni.aed.tda.listTDA;
-
-public interface IteratorTDA<E> {
-    public boolean hasNext();
-    public E next() throws NoSuchElementException;    
-}
