4b8cd6130f25430830d808676234d8c1ca6e62fc
zcodlab <zmamanir@uni.edu.pe>
2025-05-26 19:21:56 -0500

    add ArrayQueueTDA class

--- src/main/java/uni/aed/queueTDA/ArrayQueueTDA.java
+++ src/main/java/uni/aed/queueTDA/ArrayQueueTDA.java
@@ -1,35 +1,87 @@
 package uni.aed.queueTDA;
 
 public class ArrayQueueTDA<E> implements QueueTDA<E> {
+    private static final int TAMANIO_INDEFINIDO=5;
+    private E[] queue;
+    private int front;
+    private int back;
+    private int count;
 
+    public ArrayQueueTDA(int tamanio) {
+        if(tamanio<0)
+            throw new IllegalArgumentException("Capacidad debe ser mayor a cero");
+        queue=(E[])new Object[tamanio];
+        count=0;
+    }
+    
     @Override
     public void enqueue(E data) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if(count==queue.length)
+            expande();
+        queue[back]=data;
+        back=(back+1)%queue.length;
+        count++;
+    }
+    private void expande(){
+        int newLength=(int)(1.5*queue.length);
+        E[] temp=(E[])new Object[newLength];
+        int q_idx=front;
+        int t_idx=front;
+        for(int i=0;i<queue.length;i++){
+            temp[t_idx]=queue[q_idx];
+            t_idx=(t_idx+1)%temp.length;
+            q_idx=(q_idx+1)%queue.length;
+        }
+        back=t_idx;
+        queue=temp;
     }
 
     @Override
-    public E dequeue() throws QueueEmptyExceptionTDA {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public E dequeue() throws QueueEmptyExceptionTDA {//poll, remove
+        if(isEmpty())
+            throw new QueueEmptyExceptionTDA(); 
+        E item=queue[front];
+        queue[front]=null;
+        front=(front+1)%queue.length;
+        count--;
+        return item;
     }
 
     @Override
     public E peek() throws QueueEmptyExceptionTDA {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if(isEmpty())
+            throw new QueueEmptyExceptionTDA();
+        return queue[front];
     }
 
     @Override
     public void clear() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        for(int i=0;i<count;i++)
+            queue[i]=null;
+        front=back=count=0;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return(count==0);
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return count;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder str=new StringBuilder();
+        int current=front;
+        for(int i=0;i<count;i++){        
+            if(!str.isEmpty())
+                str.append(",");
+            str.append(queue[current].toString());
+            current=(current+1)%queue.length;
+        }
+        return str.toString();
     }
     
     
--- src/main/java/uni/aed/queueTDA/QueueTDAMain.java
+++ src/main/java/uni/aed/queueTDA/QueueTDAMain.java
@@ -4,8 +4,11 @@ import uni.aed.model.Persona;
 
 public class QueueTDAMain {
     public static void main(String[] args) {    
-        QueueTDAMain queueTDAMain=new QueueTDAMain();        
-        queueTDAMain.testPersonaLinkedQueueTDA();
+        QueueTDAMain queueTDAMain=new QueueTDAMain();  
+        queueTDAMain.testPersonaArrayQueueTDA();
+        //queueTDAMain.testStringArrayQueueTDA();
+        //queueTDAMain.testIntegerArrayQueueTDA();
+        //queueTDAMain.testPersonaLinkedQueueTDA();
         //queueTDAMain.testStringLinkedQueueTDA();
         //queueTDAMain.testIntegerLinkedQueueTDA();
     }
@@ -75,6 +78,74 @@ public class QueueTDAMain {
         System.out.println("Despues de Recuperar elementos: "+q.size());
         System.out.println("Visualizando elementos");
         System.out.println(q.toString());  
+    }
     
+    private void testIntegerArrayQueueTDA(){
+        QueueTDA<Integer> q=new ArrayQueueTDA(4);
+        q.enqueue(30);
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(5);
+        System.out.println("Despues de Añadir elementos: "+q.size());
+        System.out.println("Visualizando la cola");
+        System.out.println(q.toString());        
+        
+        System.out.println("Eliminando elementos");
+        System.out.println("Elemento Eliminando:"+q.dequeue().toString());
+        System.out.println("Despues de Eliminar elementos: "+q.size());
+        System.out.println("Visualizando elementos");
+        System.out.println(q.toString());        
+        
+        System.out.println("Recuperando elementos");
+        System.out.println("Elemento Recuperado:"+q.peek().toString());
+        System.out.println("Despues de Recuperar elementos: "+q.size());
+        System.out.println("Visualizando elementos");
+        System.out.println(q.toString());  
+    }
+    
+    private void testStringArrayQueueTDA(){
+        QueueTDA<String> q=new ArrayQueueTDA(4);
+        q.enqueue("Jose");
+        q.enqueue("Ana");
+        q.enqueue("Juan");
+        q.enqueue("Betty");
+        System.out.println("Despues de Añadir elementos: "+q.size());
+        System.out.println("Visualizando la cola");
+        System.out.println(q.toString());        
+        
+        System.out.println("Eliminando elementos");
+        System.out.println("Elemento Eliminando:"+q.dequeue().toString());
+        System.out.println("Despues de Eliminar elementos: "+q.size());
+        System.out.println("Visualizando elementos");
+        System.out.println(q.toString());        
+        
+        System.out.println("Recuperando elementos");
+        System.out.println("Elemento Recuperado:"+q.peek().toString());
+        System.out.println("Despues de Recuperar elementos: "+q.size());
+        System.out.println("Visualizando elementos");
+        System.out.println(q.toString());  
+    }
+    private void testPersonaArrayQueueTDA(){
+        QueueTDA<Persona> q=new ArrayQueueTDA(4);
+        q.enqueue(new Persona("Jose",36));
+        q.enqueue(new Persona("Ana",46));
+        q.enqueue(new Persona("Juan",16));
+        q.enqueue(new Persona("Betty",66));
+        
+        System.out.println("Despues de Añadir elementos: "+q.size());
+        System.out.println("Visualizando la cola");
+        System.out.println(q.toString());        
+        
+        System.out.println("Eliminando elementos");
+        System.out.println("Elemento Eliminando:"+q.dequeue().toString());
+        System.out.println("Despues de Eliminar elementos: "+q.size());
+        System.out.println("Visualizando elementos");
+        System.out.println(q.toString());        
+        
+        System.out.println("Recuperando elementos");
+        System.out.println("Elemento Recuperado:"+q.peek().toString());
+        System.out.println("Despues de Recuperar elementos: "+q.size());
+        System.out.println("Visualizando elementos");
+        System.out.println(q.toString());  
     }
 }
